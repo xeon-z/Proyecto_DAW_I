4b0a8adb18e01c4f2b26ebfc9a533eb2fad07b20
xeon-z <johncpu2k@gmail.com>
2022-11-07 00:18:14 -0500

    Eliminación de imports - UserController

--- src/main/java/com/cibertec/springboot/web/app/controllers/UserController.java
+++ src/main/java/com/cibertec/springboot/web/app/controllers/UserController.java
@@ -1,9 +1,6 @@
 package com.cibertec.springboot.web.app.controllers;
 
-import java.util.Map;
-
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,17 +8,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.cibertec.springboot.web.app.models.entity.Libro;
 import com.cibertec.springboot.web.app.models.entity.Usuario;
 import com.cibertec.springboot.web.app.models.service.ILibroService;
 import com.cibertec.springboot.web.app.models.service.IUsuarioService;
